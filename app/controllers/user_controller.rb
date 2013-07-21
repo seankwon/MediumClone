a28899a7a6cac452dee0ddc030003d0c4d2d63f2
seankwn@live.com <seankwn@live.com>
2013-07-21 17:43:25 -0400

    broken code

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,16 @@ class UserController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if passwordConfirmation == params[:user][:password] && params[:user].each.any?
+      redirect_to root
+    else 
+      redirect_to user_new_path
+    end
   end
+
+  private 
+    def user_params
+       params.require(:user).permit(:name, :email, :password)
+    end
 end
