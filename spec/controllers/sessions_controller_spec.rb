d8748afa28bf4f028af23c4d509aed93311fcdc6
seankwn@live.com <seankwn@live.com>
2013-07-24 23:11:03 -0400

    capybara, i hate you

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
-describe SessionsController do
+describe 'SessionsController' do
 
   describe 'Login page' do
-    before { visit login_path }
-    it { should have_link('Submit') }
-    it { should have_selector('h1', text: 'Login') }
+
+    it 'should do something cool' do
+      visit login_path
+      expect(page).to have_selector('h1', text: 'Login')
+    end
+
   end
 
   
