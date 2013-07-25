024724f0ec8c9a7102946aa0e7e077298968776b
seankwn@live.com <seankwn@live.com>
2013-07-25 19:25:18 -0400

    i can do tests finallygit add .

--- spec/controllers/genrepages_controller_spec.rb
+++ spec/controllers/genrepages_controller_spec.rb
@@ -5,12 +5,12 @@ describe 'GenrepagesController' do
   describe 'Home Page' do
     it 'should have the h1 "Medium platform"' do
       visit root_path
-      expect(page).to have_content('Medium platform')
+      page.should have_content('Medium platform')
     end
 
     it 'should have a link redirecting to sign in' do
       visit root_path
-      expect(page).to have_link('Sign in!')
+      page.should have_link('Sign in!')
     end
   end
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,12 +4,19 @@ describe 'SessionsController' do
 
   describe 'Login page' do
 
-    it 'should do something cool' do
+    it 'should have content' do
       visit login_path
-      expect(page).to have_selector('h1', text: 'Login')
+      page.should have_selector('h1', 'Login')
+      page.should have_css('input.user-input')
+    end
+
+    it 'should be able to fill in two text-fields' do
+      visit login_path
+      fill_in "email", :with => 'example'
+      fill_in "password", :with => 'foobar'
+      click_button "Submit"
     end
 
   end
-
   
 end
