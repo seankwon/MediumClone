74f69f58b20d03a6124ac3a92c6e0b8d90721079
seankwn@live.com <seankwn@live.com>
2013-08-04 16:53:11 -0400

    please work

--- db/migrate/20130730022952_remove_genre_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveGenreFromPosts < ActiveRecord::Migration
-  def change
-    remove_column :posts, :genre, :string
-  end
-end
--- db/migrate/20130731024305_drop_posts_tables.rb
+++
@@ -1,5 +0,0 @@
-class DropPostsTables < ActiveRecord::Migration
-  def change
-    drop_table :posts
-  end
-end
